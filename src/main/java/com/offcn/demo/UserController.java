b3a00f77091acf890853a0474ba29d75648dbbff
offcn-java <772640952@qq.com>
2020-09-25 21:53:21 +0800

    测试项目

---
+++ src/main/java/com/offcn/demo/UserController.java
@@ -0,0 +1,17 @@
+package com.offcn.demo;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 获取用户名
+ */
+@RestController
+public class UserController {
+    @RequestMapping("/findLoginUser")
+    public void  findLoginUser(){
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        System.out.println(name);
+    }
+}
